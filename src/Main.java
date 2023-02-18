2f952f0d5fba59c92d03edf87f306bc5adf56a22
Marina <m-fisun23@yandex.ru>
2023-02-19 00:37:33 +0300

    Task 8

--- src/Main.java
+++ src/Main.java
@@ -18,40 +18,57 @@ public class Main {
             System.out.println(i);
         }
     }
+
     public static void task2() {
         System.out.println("Task 2");
         for (int i = 10; i >= 1; i = i--) {
             System.out.println(i--);
         }
     }
+
     public static void task3() {
-            System.out.println("Task 3");
-            for (int i=0; i < 17; i = i+2) {
-                System.out.println(i);
-            }
+        System.out.println("Task 3");
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(i);
+        }
     }
+
     public static void task4() {
         System.out.println("Task 4");
-        for (int i = 10; i >-11; i =i--) {
+        for (int i = 10; i > -11; i = i--) {
             System.out.println(i--);
         }
     }
+
     public static void task5() {
         System.out.println("Task 5");
-        for (int i = 1904; i <= 2096; i = i +4) {
+        for (int i = 1904; i <= 2096; i = i + 4) {
             System.out.println(i + " год является високосным");
         }
     }
+
     public static void task6() {
         System.out.println("Task 6");
-        for (int i = 7; i <= 98; i = i+7) {
+        for (int i = 7; i <= 98; i = i + 7) {
             System.out.println(i);
         }
     }
+
     public static void task7() {
         System.out.println("Task 7");
-        for (int i = 1; i <= 512; i = i*2) {
+        for (int i = 1; i <= 512; i = i * 2) {
             System.out.println(i);
         }
     }
+
+    public static void task8() {
+        System.out.println("Task 8");
+        int sum = 0;
+        int part = 29000;
+        for (int i = 1; i <= 12; i++) {
+            sum+=part;
+            System.out.println("Месяц " + i+ " сумма накоплений равна " + sum + " рублей");
+        }
+    }
+
 }
